7c59d1b968c647fe3ec1d39c8dd6e637c9c5b352
Ivan <ivanpiccioni.work@gmail.com>
2023-11-30 11:54:11 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Studente studente1 = new Studente("Ivan","Piccioni",28);
+
+
+        System.out.println("I dati dello studente sono:" +studente1);
+    }
+
+}
+
